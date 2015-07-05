b498ad7d2eafb9765ed8bcc5cb9d22e4390480f8
shopetan <s.kikuchi1120@gmail.com>
2015-07-05 21:42:20 +0900

    check heroku

--- app/controllers/opac_controller.rb
+++ app/controllers/opac_controller.rb
@@ -1,26 +1,23 @@
 # coding: utf-8
 class OpacController < ApplicationController
-  
-  def search
 
+  def search
     # get query
     @query = Opac.ransack(params[:q])
     @flag = params[:q] ? false : true
     @page ||= '1'
     @hits = @query.result.size
     @opac = @query.result.page(params[:page])
-    
+
     respond_to do |format|
-      format.html 
+      format.html
       format.json {render json:@opac}
     end
   end
 
   def detail
-    
     # NBC
     nbc = params[:nbc]
-
     # get opac data
     @opac = Opac.where({'nbc'=>nbc})
   end
